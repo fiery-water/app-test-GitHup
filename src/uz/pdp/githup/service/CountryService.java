46f03a86bcea70eeb9f550247bd82ae2cf2dbad7
Mavlonbek <mavlonbektoshtemirov54@gmail.com>
2023-11-09 20:23:17 +0500

    first

---
+++ src/uz/pdp/githup/service/CountryService.java
@@ -0,0 +1,7 @@
+package uz.pdp.githup.service;
+
+public class CountryService {
+    public static void main(String[] args) {
+        System.out.println("CountryServiceda kod yozilyapti");
+    }
+}
---
+++ src/uz/pdp/githup/service/PersonService.java
@@ -0,0 +1,7 @@
+package uz.pdp.githup.service;
+
+public class PersonService {
+    public static void main(String[] args) {
+        System.out.println("Person serviceda kod yozyapman");
+    }
+}
